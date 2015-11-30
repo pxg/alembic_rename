a900416f1c60ada076e65b265f863493fd7d6d66
Pete Graham <git@petegraham.co.uk>
2015-11-30 10:27:26 +0000

    Alembic init

--- models.py
+++ models.py
@@ -1,22 +1,9 @@
-from sqlalchemy import create_engine, Column, Integer, DateTime, String
+from sqlalchemy import create_engine, Column, Integer, DateTime
 from sqlalchemy.ext.declarative import declarative_base
 
 
 Base = declarative_base()
 engine = create_engine('postgresql://snacks@localhost/snacks', echo=True)
-
-
-# class User(Base):
-#     __tablename__ = 'users'
-
-#     id = Column(Integer, primary_key=True)
-#     name = Column(String)
-#     fullname = Column(String)
-#     password = Column(String)
-
-#     def __repr__(self):
-#         return "<User(name='%s', fullname='%s', password='%s')>" % (
-#             self.name, self.fullname, self.password)
 
 
 class Marathon(Base):
